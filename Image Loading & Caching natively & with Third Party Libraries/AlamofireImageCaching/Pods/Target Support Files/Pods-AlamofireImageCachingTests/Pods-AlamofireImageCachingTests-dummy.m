32465aa2cfbbba8a8fac7f5395bdd7d5b48fc094
Kenny Batista <kennybatista7@gmail.com>
2017-02-15 11:59:48 -0800

    Image loading and caching

---
+++ Image Loading & Caching natively & with Third Party Libraries/AlamofireImageCaching/Pods/Target Support Files/Pods-AlamofireImageCachingTests/Pods-AlamofireImageCachingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlamofireImageCachingTests : NSObject
+@end
+@implementation PodsDummy_Pods_AlamofireImageCachingTests
+@end
---
+++ Image Loading & Caching natively & with Third Party Libraries/NativeImageCaching/NativeImageCaching/MainCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainCollectionViewCell.swift
+//  NativeImageCaching
+//
+//  Created by Kenny Batista on 2/15/17.
+//  Copyright © 2017 kennybatista. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+}
