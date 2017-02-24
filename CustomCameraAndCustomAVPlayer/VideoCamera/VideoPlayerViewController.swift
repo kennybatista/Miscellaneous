c3d0105571e748a6f4821cc45bf1b31f36669b97
Kenny Batista <kennybatista7@gmail.com>
2017-02-23 23:39:53 -0800

    working on player controllers

--- CustomCameraAndCustomAVPlayer/VideoCamera/VideoPlayerViewController.swift
+++ CustomCameraAndCustomAVPlayer/VideoCamera/VideoPlayerViewController.swift
@@ -30,9 +30,20 @@ class VideoPlayerViewController: UIViewController {
     //player layer 
     fileprivate var playerLayer: AVPlayerLayer!
     
-    
+    // Time Remaining
     fileprivate var timeObserver: AnyObject!
     fileprivate var timeRemainingLabel: UILabel!
+    
+    // Seeker buttons
+    var playerRateBeforeSeek: Float = 0
+    var seekFiveSecondsFowardButton: UIButton!
+    var seekFiveSecondsBackwardButton: UIButton!
+    var playOrPauseButton: UIButton!
+    var playPauseBool: Bool!
+    
+    
+
+
     
 // [End - Properties] ---------Properties-----------Properties-------------Properties---------------Properties----------
 
@@ -61,17 +72,21 @@ class VideoPlayerViewController: UIViewController {
         loadView()
         player.play()
         
-        
+        // time remaining
         let timeInterval: CMTime = CMTimeMakeWithSeconds(1.0, 10)
         timeObserver = player.addPeriodicTimeObserver(forInterval: timeInterval, queue: DispatchQueue.main, using: {
             elapsedTime in
             print("elapsedTime now", CMTimeGetSeconds(elapsedTime))
             self.observeTime(elapsedTime: elapsedTime)
+            
+            
+            
         }) as AnyObject!
         
         
         
         
+        playPauseBool = true
         
     }
     
@@ -111,26 +126,102 @@ class VideoPlayerViewController: UIViewController {
         view.addSubview(timeRemainingLabel)
         
         
-    }
-    
-    
-    
-    fileprivate func updateTimeLabel(elapsedTime: Float64, duration: Float64) {
-        let timeRemaining: Float64 = CMTimeGetSeconds(player.currentItem!.duration) - elapsedTime
-        timeRemainingLabel.text = String(format: "%02d:%02d", ((lround(timeRemaining) / 60) % 60), lround(timeRemaining) % 60)
-    }
-    
-    fileprivate func observeTime(elapsedTime: CMTime) {
-        let duration = CMTimeGetSeconds((player.currentItem?.duration)!)
-        let elapsedTime = CMTimeGetSeconds(elapsedTime)
-        updateTimeLabel(elapsedTime: elapsedTime, duration: duration)
+        seekFiveSecondsFowardButton = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 100))
+        seekFiveSecondsBackwardButton = UIButton(frame: CGRect(x: 150, y: 200, width: 100, height: 100))
+        playOrPauseButton = UIButton(frame: CGRect(x: 250, y: 300, width: 100, height: 100))
+        
+        seekFiveSecondsFowardButton.setTitle("Forward", for: .normal)
+        seekFiveSecondsBackwardButton.setTitle("Backward", for: .normal)
+        playOrPauseButton.setTitle("PlayPause", for: .normal)
+        
+        seekFiveSecondsFowardButton.addTarget(self, action: #selector(VideoPlayerViewController.seekForward(button:)), for: .touchUpInside)
+        seekFiveSecondsBackwardButton.addTarget(self, action: #selector(VideoPlayerViewController.seekBackward(button:)), for: .touchUpInside)
+        playOrPauseButton.addTarget(self, action: #selector(VideoPlayerViewController.pauseOrPlay(button:)), for: .touchUpInside)
+        
+        view.addSubview(seekFiveSecondsFowardButton)
+        view.addSubview(seekFiveSecondsBackwardButton)
+        view.addSubview(playOrPauseButton)
         
     }
     
     
+    // Start - Time Remaining Methods
     
     
+    fileprivate func observeTime(elapsedTime: CMTime) {
+        let duration = CMTimeGetSeconds((player.currentItem?.duration)!)
+        let elapsedTime = CMTimeGetSeconds(elapsedTime)
+        
+        //updtae time label
+        updateTimeLabel(elapsedTime: elapsedTime, duration: duration)
+        
+        //seek forward/backwards
+    }
     
+    fileprivate func updateTimeLabel(elapsedTime: Float64, duration: Float64) {
+        let timeRemaining: Float64 = CMTimeGetSeconds(player.currentItem!.duration) - elapsedTime
+        timeRemainingLabel.text = String(format: "%02d:%02d", ((lround(timeRemaining) / 60) % 60), lround(timeRemaining) % 60)
+        
+    }
+    
+    // End - Time Remaining Methods
+    
+    
+    //Start - Seeker Methods
+    
+//    @objc private func sliderBeganTracking(slider: UISlider) {
+//        playerRateBeforeSeek = player.rate
+//        player.pause()
+//    }
+//    
+//    @objc fileprivate func sliderEndedTracking(slider: UISlider) {
+//        let videoDuration = CMTimeGetSeconds((player.currentItem?.duration)!)
+//        let elapsedTime: Float64 = videoDuration * Float64(seekSlider.value)
+//        updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
+//        
+//        player.seek(to: CMTimeMakeWithSeconds(elapsedTime, 100), completionHandler: { (completed: Bool) -> Void in
+//            if self.playerRateBeforeSeek > 0 {
+//                self.player.play()
+//            }
+//
+//        })
+//        
+//    }
+//    
+//    @objc fileprivate func sliderValueChanged(slider: UISlider) {
+//        let videoDuration = CMTimeGetSeconds((player.currentItem?.duration)!)
+//        let elapsedTime: Float64 = videoDuration * Float64(seekSlider.value)
+//        updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
+//    }
+    
+    fileprivate func currentPlayBackTime() -> CMTime {
+        return (player.currentItem?.currentTime())!
+    }
+    
+    @objc fileprivate func seekForward(button: UIButton, elapsedTime: CMTime){
+        print(#function)
+        
+        self.player.seek(to: CMTime(5))
+    }
+    
+    @objc fileprivate func seekBackward(button: UIButton, elapsedTime: CMTime){
+        print(#function)
+        
+    }
+    
+    @objc fileprivate func pauseOrPlay(button: UIButton){
+        print(#function)
+            if playPauseBool == false {
+                playPauseBool = true
+                player.pause()
+            } else if playPauseBool == true {
+                playPauseBool = false
+                player.play()
+            }
+    }
+    
+    
+    //End - Seeker Methods
     
 
     override func didReceiveMemoryWarning() {
